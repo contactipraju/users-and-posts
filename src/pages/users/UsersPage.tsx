52a0fb362f0e6312036e27410058fda0a831bb47
Prasada Indukuri <prasada.indukuri_migration@specialized.com>
2023-06-15 02:07:33 +1000

    loading users in the UsersPage, passing the same thru props and displaying them in Users component

--- src/pages/users/UsersPage.tsx
+++ src/pages/users/UsersPage.tsx
@@ -1,11 +1,23 @@
+import { useState, useEffect } from "react";
+
+import { getUsers } from "./Users.service";
+
 import Users from './components/Users/Users';
 
 import './UsersPage.scss';
 
 function UsersPage() {
+	const [users, setUsers] = useState([]);
+
+	useEffect(() => {
+		getUsers().then((resp) => {
+			setUsers(resp);
+		})
+	}, []);
+
 	return (
 		<div id="users-page">
-			<Users />
+			<Users users={users}/>
 		</div>
 	);
 }
--- src/pages/users/components/Users/Users.tsx
+++ src/pages/users/components/Users/Users.tsx
@@ -1,9 +1,38 @@
 import './Users.scss';
 
+import { IUserInfo } from '../../Users.interfaces';
+
 function Users(props: any) {
+
 	return (
 		<div id="users-and-posts">
-			List Users here...
+			{
+				props.users && props.users.length > 0 && 
+				<div className="users">
+					<table>
+						<thead>
+							<tr>
+								<th>Name</th>
+								<th>Email</th>
+								<th>City</th>
+								<th>Company</th>
+							</tr>
+						</thead>
+						<tbody>
+						{
+							props.users.map((user: IUserInfo, index: number) => (
+								<tr key={user.id}>
+									<td>{user.name}</td>
+									<td>{user.email}</td>
+									<td>{user.address.city}</td>
+									<td>{user.company.name}</td>
+								</tr>
+							))
+						}
+						</tbody>
+					</table>
+				</div>
+			}
 		</div>
 	)
 }
