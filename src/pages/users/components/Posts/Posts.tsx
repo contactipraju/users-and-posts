c8aebf0ff15373d5115070429d9713c1c56af4be
Prasada Indukuri <prasada.indukuri_migration@specialized.com>
2023-06-19 16:44:28 +1000

    using useCallback (instead of useEffect) hook to load/reload posts based on selected user

--- src/pages/users/components/Posts/Posts.tsx
+++ src/pages/users/components/Posts/Posts.tsx
@@ -1,12 +1,12 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from 'react';
 import { useSelector } from 'react-redux';
 
+import store         from '../../store/store';
 import { IStore }    from '../../store/store';
 import { IPostInfo } from '../../Users.interfaces';
 import { getPosts }  from '../../Users.service';
 
-import store from './../../store/store';
-import { updateSelectedUser } from "../../store/actions";
+import { updateSelectedUser } from '../../store/actions';
 
 import './Posts.scss';
 
@@ -14,68 +14,70 @@ function Posts() {
 	const [posts, setPosts] = useState([]);
 	const selectedUser = useSelector((state: IStore) => state.selectedUser!);
 
+	const reLoadPosts = useCallback(async (id:number) => {
+		try {
+			const response = await getPosts(id);
+			setPosts(response);
+		} catch (e) {
+			console.error(e);
+		}
+	}, []);
+
 	useEffect(() => {
 		if (selectedUser && selectedUser.id >= 0) {
-            getPosts(selectedUser.id).then((resp) => {
-                //console.log(`Loaded posts for: ${selectedUser.id}`, resp);
-                setPosts(resp);
-            })
+			reLoadPosts(selectedUser.id);
 		} else {
 			setPosts([]);
 		}
-	}, [selectedUser]);
-
-	// TODO: Fix code duplication.
-	// It's not adviced to call outside functions from within useEffect.
-	// May be check useMemo or useCallback?
-	function reLoadPosts() {
-		getPosts(selectedUser.id).then((resp) => {
-			//console.log(`Reloaded posts for: ${selectedUser.id}`, resp);
-			setPosts(resp);
-		})
-	}
+	}, [selectedUser, reLoadPosts]);
 
 	return (
-		<div id="posts">
-			{
-				selectedUser && selectedUser.name ? 
-				<>
-					<div>
-						<span data-testid="displaying-posts">Displaying posts for the user:</span>
-						<span>{selectedUser.name}</span>
-					</div>
-					<div>
-						<button aria-label="reload" onClick={ () => reLoadPosts()}>Reload</button>
-						<button aria-label="clear" onClick={ () => store.dispatch(updateSelectedUser(null))}>Clear Selection</button>
-					</div>
-				</>
-				: <span data-testid="select-user">Select a user to fetch the posts</span>
-			}
-			{
-				posts && posts.length > 0 && 
-				<div className="posts">
-					<table>
-						<thead>
-							<tr>
-								<th>Title</th>
-								<th>Body</th>
-							</tr>
-						</thead>
-						<tbody>
-							{
-								posts.map((post: IPostInfo) => (
-									<tr key={post.id}>
-										<td>{post.title}</td>
-										<td>{post.body}</td>
-									</tr>
-								))
-							}
-						</tbody>
-					</table>
+		<div id='posts'>
+		{selectedUser && selectedUser.name ? (
+			<>
+				<div>
+					<span data-testid='displaying-posts'>
+					Displaying posts for the user:
+					</span>
+					<span>{selectedUser.name}</span>
 				</div>
-			}
+				<div>
+					<button aria-label='reload' onClick={() => reLoadPosts(selectedUser.id)}>
+					Reload
+					</button>
+					<button
+					aria-label='clear'
+					onClick={() => store.dispatch(updateSelectedUser(null))}
+					>
+					Clear Selection
+					</button>
+				</div>
+			</>
+		) : (
+			<span data-testid='select-user'>Select a user to fetch the posts</span>
+		)}
+		{posts && posts.length > 0 && (
+			<div className='posts'>
+				<table>
+					<thead>
+						<tr>
+							<th>Title</th>
+							<th>Body</th>
+						</tr>
+					</thead>
+					<tbody>
+						{posts.map((post: IPostInfo) => (
+							<tr key={post.id}>
+							<td>{post.title}</td>
+							<td>{post.body}</td>
+							</tr>
+						))}
+					</tbody>
+				</table>
+			</div>
+		)}
 		</div>
-	)
+	);
 }
 
 export default Posts;
