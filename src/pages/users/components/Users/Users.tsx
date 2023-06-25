d509446fdc4a58f4103a7e2da0b2874f3618c074
Prasada Indukuri <prasada.indukuri_migration@specialized.com>
2023-06-26 06:01:02 +1000

    sorting (asc/desc) the table based on column selection (w3schools way)

--- src/pages/users/components/Users/Users.tsx
+++ src/pages/users/components/Users/Users.tsx
@@ -6,20 +6,77 @@ import { IUserInfo } from '../../Users.interfaces';
 import './Users.scss';
 
 function Users({filteredUsers}: any) {
+	const sortTable = (n: any) => {
+		console.log('sortTable: ', n);
+		let table: any;
+		let rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+		table = document.getElementById("users-table");
+		switching = true;
+		// Set the sorting direction to ascending:
+		dir = "asc";
+		/* Make a loop that will continue until
+		no switching has been done: */
+		while (switching) {
+		  // Start by saying: no switching is done:
+		  switching = false;
+		  rows = table!.rows;
+		  /* Loop through all table rows (except the
+		  first, which contains table headers): */
+		  for (i = 1; i < (rows.length - 1); i++) {
+			// Start by saying there should be no switching:
+			shouldSwitch = false;
+			/* Get the two elements you want to compare,
+			one from current row and one from the next: */
+			x = rows[i].getElementsByTagName("TD")[n];
+			y = rows[i + 1].getElementsByTagName("TD")[n];
+			/* Check if the two rows should switch place,
+			based on the direction, asc or desc: */
+			if (dir === "asc") {
+			  if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+				// If so, mark as a switch and break the loop:
+				shouldSwitch = true;
+				break;
+			  }
+			} else if (dir === "desc") {
+			  if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+				// If so, mark as a switch and break the loop:
+				shouldSwitch = true;
+				break;
+			  }
+			}
+		  }
+		  if (shouldSwitch) {
+			/* If a switch has been marked, make the switch
+			and mark that a switch has been done: */
+			rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+			switching = true;
+			// Each time a switch is done, increase this count by 1:
+			switchcount ++;
+		  } else {
+			/* If no switching has been done AND the direction is "asc",
+			set the direction to "desc" and run the while loop again. */
+			if (switchcount === 0 && dir === "asc") {
+			  dir = "desc";
+			  switching = true;
+			}
+		  }
+		}
+	};
+
 	return (
 		<div id='filtered-users'>
 			{
 				filteredUsers && filteredUsers.length > 0 && 
 				<div className='users'>
-					<table>
+					<table id="users-table">
 						<thead>
 							<tr>
-								<th>Name</th>
-								<th>Username</th>
-								<th>Email</th>
-								<th>Full Address</th>
-								<th>Company</th>
-								<th>Website</th>
+								<th onClick={() => sortTable(0)}>Name</th>
+								<th onClick={() => sortTable(1)}>Username</th>
+								<th onClick={() => sortTable(2)}>Email</th>
+								<th onClick={() => sortTable(3)}>Full Address</th>
+								<th onClick={() => sortTable(4)}>Company</th>
+								<th onClick={() => sortTable(5)}>Website</th>
 							</tr>
 						</thead>
 						<tbody>
